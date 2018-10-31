fdbd7fe7fba27bf4f6afa7ffb5f90ccdbde793d8
Aidy <microsoft@aidanjenkins.co.uk>
2018-10-31 23:58:52 +0000

    20181031 ATJ Initial commit of windows solution

---
+++ remote-lock-win/remote-lock-win/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace remote_lock_win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
