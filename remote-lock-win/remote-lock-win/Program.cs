4fdc386a91c7875123073558affd7c54278ebe00
Aidy <microsoft@aidanjenkins.co.uk>
2018-11-01 00:18:57 +0000

    20181101 ATJ Created proof of concept program that locks session based on user choice

--- remote-lock-win/remote-lock-win/Program.cs
+++ remote-lock-win/remote-lock-win/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace remote_lock_win
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if (DialogResult.Yes == MessageBox.Show("Would you like to lock the current session?", "Lock Session", MessageBoxButtons.YesNo))
+                Win32.LockSession();
         }
     }
 }
---
+++ remote-lock-win/remote-lock-win/Win32.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace remote_lock_win
+{
+    public class Win32
+    {
+        private static void Call(Func<bool> callback)
+        {
+            if (callback())
+                return;
+
+            throw Marshal.GetExceptionForHR(Marshal.GetLastWin32Error());
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool LockWorkStation();
+
+        public static void LockSession() => Call(LockWorkStation);
+    }
+}
